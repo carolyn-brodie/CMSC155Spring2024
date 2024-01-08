7e85cf6619f7eccc34aef55e68785a87cba9076d
carolyn-brodie <48027983+carolyn-brodie@users.noreply.github.com>
2024-01-07 21:30:21 -0600

    More examples

---
+++ src/week1/TryNumbers.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class TryNumbers {
+    public static void main(String[] args) {
+        int number;
+        number = 2 + 3 * 4;
+        System.out.print("My favorite number is ");
+        System.out.println(number + ".");
+
+    }
+}
